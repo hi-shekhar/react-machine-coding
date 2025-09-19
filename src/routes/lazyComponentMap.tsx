95f9c41048e779f23f3ff79b44a5fb34181bd483
hi-shekhar <himanshu26592@gmail.com>
2025-09-19 18:39:18 +0200

    form validation

--- src/routes/lazyComponentMap.tsx
+++ src/routes/lazyComponentMap.tsx
@@ -3,9 +3,11 @@ import { lazy } from "react";
 const CounterApp = lazy(() => import("../components/CounterApp"));
 const TodoListApp = lazy(() => import("../components/TodoListApp"));
 const StopwatchApp = lazy(() => import("../components/StopwatchApp"));
+const FormValidationApp = lazy(() => import("../components/FormValidationApp"));
 
 export const componentMap: Record<string, React.FC> = {
   "project-1": CounterApp,
   "project-2": TodoListApp,
   "project-3": StopwatchApp,
+  "project-9": FormValidationApp,
 };
